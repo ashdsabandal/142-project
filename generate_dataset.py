05c28ce33b121783ac0a4c8d5b6a24485dd781a4
ashdsabandal <adsabandal@up.edu.ph>
2026-04-26 20:59:55 +0800

    feat: add dataset files and update dataset generation script

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,21 +1,20 @@
 import random
 
-
-N = 1000
+N = 7
 print("=== Generate a dataset of size x * N ===")
 print("N =", N)
 x = int(input("Enter x: "))
 total_n = x * N
 
 value_min = 1
-value_max = 500
-values = [random.randint(value_min, value_max) for _ in range(total_n)] # random values from 1 to 500
+value_max = 20
+values = [random.randint(value_min, value_max) for _ in range(total_n)] # random values from 1 to 20
 
 weight_min = 1
-weight_max = 50
-weights = [random.randint(weight_min, weight_max) for _ in range(total_n)] # random weights from 1 to 50
+weight_max = 10
+weights = [random.randint(weight_min, weight_max) for _ in range(total_n)] # random weights from 1 to 10
 
-W = total_n // 2  # half of N
+W = total_n * 2  # half of N
 
 with open(f"{x}x.txt", 'w') as f:
     f.write(f"n {total_n}\n")
